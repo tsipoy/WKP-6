0ef6d5b02fd52bad09827e9b28b47b6f78e94121
Natacha <natacha.har@onja.org>
2020-08-08 09:39:31 +0300

    Added new files

---
+++ script.js
@@ -0,0 +1,37 @@
+const foods = [
+    {
+        id: 'ravitoto',
+        price: 5000,
+        title: 'Ravitoto',
+        spicy: true,
+        vegetarian: false,
+    },
+    {
+        id: 'pasta',
+        price: 4000,
+        title: 'Pasta',
+        spicy: true,
+        vegetarian: true,
+    },
+    {
+        id: 'burger',
+        price: 5000,
+        title: 'Burger',
+        spicy: false,
+        vegetarian: false,
+    },
+    {
+        id: 'rice',
+        price: 2000,
+        title: 'Rice and Leaves',
+        spicy: false,
+        vegetarian: true,
+    },
+    {
+        id: 'mofogasy',
+        price: 500,
+        title: 'Mofogasy',
+        spicy: false,
+        vegetarian: false,
+    },
+];
